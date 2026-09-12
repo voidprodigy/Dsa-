74ef9970921a82bfd70fe704baca803dd1bf9d9e
Pratik Kumar <pratikkumar232005@gmail.com>
2026-09-12 23:19:39 +0530

    Time: 3 ms (76.61%) | Memory: 46.2 MB (85.14%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int romanToInt(String s) {
+        int total = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            int current = value(s.charAt(i));
+
+            // If current value is smaller than next value, subtract it
+            if (i < s.length() - 1 && current < value(s.charAt(i + 1))) {
+                total -= current;
+            } else {
+                total += current;
+            }
+        }
+
+        return total;
+    }
+
+    private int value(char ch) {
+        switch (ch) {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+        }
+
+        return 0;
+    }
+}
